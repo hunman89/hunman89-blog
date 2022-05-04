3636be4c27635431f281377e787293a98f2de5f5
hunman89 <hunman89@gmail.com>
2022-05-04 15:41:29 +0900

    fix. 사이드 겹침 수정

--- src/components/contentsTable.tsx
+++ src/components/contentsTable.tsx
@@ -2,39 +2,39 @@ import { Link } from "gatsby";
 import React from "react";
 
 export interface IListItem {
-    url: string;
-    title: string;
-    items: IListItem[];
+  url: string;
+  title: string;
+  items: IListItem[];
 }
 
 const ContentsItem = ({ item }: { item: IListItem }) => (
-    <li>
-        <a className=" text-gray-500 hover:text-gray-700" href={item.url}>
-            {item.title}
-        </a>
-        {item.items && item.items.length && (
-            <ContentsList key={`${item.url}-list`} items={item.items} />
-        )}
-    </li>
+  <li>
+    <a className=" text-gray-500 hover:text-gray-700" href={item.url}>
+      {item.title}
+    </a>
+    {item.items && item.items.length && (
+      <ContentsList key={`${item.url}-list`} items={item.items} />
+    )}
+  </li>
 );
 
 const ContentsList = ({ items }: { items: IListItem[] }) => {
-    return (
-        <ul className="pl-3 py-1">
-            {items.map((item: IListItem) => {
-                return <ContentsItem key={`${item.url}-item`} item={item} />;
-            })}
-        </ul>
-    );
+  return (
+    <ul className="pl-3 py-1">
+      {items.map((item: IListItem) => {
+        return <ContentsItem key={`${item.url}-item`} item={item} />;
+      })}
+    </ul>
+  );
 };
 
 const ContentsTable = ({ data }: { data: IListItem[] }) => {
-    return (
-        <div className=" invisible xl:visible fixed xl:right-64 xl:top-64 h-60 overflow-hidden">
-            <h2 className="dark:text-white pb-3">Table of Contents</h2>
-            <ContentsList items={data} />
-        </div>
-    );
+  return (
+    <div className=" invisible 2xl:visible fixed 2xl:right-[5%] xl:top-64 h-60 overflow-hidden">
+      <h2 className="dark:text-white pb-3">Table of Contents</h2>
+      <ContentsList items={data} />
+    </div>
+  );
 };
 
 export default ContentsTable;
--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -2,24 +2,24 @@ import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
 const Profile = () => {
-    return (
-        <div className="fixed left-64 top-64">
-            <div className=" flex items-center justify-center">
-                <StaticImage
-                    className=" w-32 aspect-square shadow-md rounded-full"
-                    src="../images/hunman.jpg"
-                    alt="hunman image"
-                />
-            </div>
-            <div className=" sm:space-y-5 pt-5 text-sm text-gray-700 w-full dark:text-white">
-                <div>안녕하세요👏</div>
-                <div>
-                    개발자 <span className="hover:text-purple-700">정성훈</span>
-                    입니다.
-                </div>
-            </div>
+  return (
+    <div className="fixed 2xl:left-[5%] 2xl:visible invisible top-64">
+      <div className=" flex items-center justify-center">
+        <StaticImage
+          className=" w-44 aspect-square shadow-md rounded-full"
+          src="../images/hunman.jpg"
+          alt="hunman image"
+        />
+      </div>
+      <div className=" space-y-5 pt-5 text-lg text-gray-700 w-full dark:text-white">
+        <div>안녕하세요👏</div>
+        <div>
+          개발자 <span className="hover:text-purple-700">정성훈</span>
+          입니다.
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
